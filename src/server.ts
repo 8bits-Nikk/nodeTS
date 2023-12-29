7c7b1da49d98e357258898c355a2423183b88e6e
8bits-Nikk <nikhilmakwana028@gmail.com>
2023-12-29 18:18:57 +0530

    initial commit

---
+++ src/server.ts
@@ -0,0 +1,15 @@
+import "dotenv/config";
+// import mongoose from "mongoose";
+
+// async function connectDatabase() {
+//   console.log(process.env.DB_URL);
+//   await mongoose.connect(process.env.DB_URL);
+// }
+
+// connectDatabase()
+//   .then(() => {
+//     console.log("connected");
+//   })
+//   .catch((err) => console.log("error: ", err));
+
+console.log("Hello World!");
